556d846583e299b5a1d67a95389b72ff98c41829
Andrew Klaus <andrewklaus@gmail.com>
2017-06-17 11:58:11 -0600

    New version of alpine

--- alpine-armhf/fetch_rootfs.sh
+++ alpine-armhf/fetch_rootfs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -L https://nl.alpinelinux.org/alpine/v3.6/releases/armhf/alpine-minirootfs-3.6.1-armhf.tar.gz -o rootfs.tar.gz
+curl -L https://nl.alpinelinux.org/alpine/v3.6/releases/armhf/alpine-minirootfs-3.6.2-armhf.tar.gz -o rootfs.tar.gz
