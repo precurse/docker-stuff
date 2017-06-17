688eb8e1f9c194db6f692e6ce665e7188114c7fd
Andrew Klaus <andrewklaus@gmail.com>
2017-06-17 12:31:53 -0600

    Adding build script for alpine image

---
+++ alpine-armhf/build.sh
@@ -0,0 +1,13 @@
+set -ex
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=precurse
+# image name
+IMAGE=alpine-armhf
+VERSION=`cat VERSION`
+
+curl -L https://nl.alpinelinux.org/alpine/v${VERSION:0:2}/releases/armhf/alpine-minirootfs-${VERSION}-armhf.tar.gz -o rootfs.tar.gz
+
+docker build -t $USERNAME/$IMAGE:latest .
+
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
--- alpine-armhf/fetch_rootfs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -L https://nl.alpinelinux.org/alpine/v3.6/releases/armhf/alpine-minirootfs-3.6.2-armhf.tar.gz -o rootfs.tar.gz
