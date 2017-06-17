9dad6d05641488f6a1d9c82cb88ed8f5fce77cb1
Andrew Klaus <andrewklaus@gmail.com>
2017-06-17 12:38:26 -0600

    Renaming to nut-armhf  and adding build script

---
+++ nut-armhf/build.sh
@@ -0,0 +1,11 @@
+set -ex
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=precurse
+# image name
+IMAGE=nut-armhf
+VERSION=`cat VERSION`
+
+docker build -t $USERNAME/$IMAGE:latest .
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+
