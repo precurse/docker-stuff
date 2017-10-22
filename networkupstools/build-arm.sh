a7fbaeaf1dd294feecbac6ed4a09a08fd35b94bf
Andrew Klaus <andrewklaus@gmail.com>
2017-10-21 23:21:05 -0600

    removing unused arg

--- networkupstools/build-arm.sh
+++ networkupstools/build-arm.sh
@@ -7,7 +7,7 @@ IMAGE=nut-armhf
 VERSION=`cat VERSION`
 BASE_IMAGE="precurse/alpine-armhf:3.6.2"
 
-docker build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg ARCH=${ARCH} -t $USERNAME/$IMAGE:latest .
+docker build --build-arg BASE_IMAGE=${BASE_IMAGE} -t $USERNAME/$IMAGE:latest .
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
 
 
