0c8d612b0f28fee30e62fa3ba75f03d73418539b
Andrew Klaus <andrewklaus@gmail.com>
2017-11-12 00:05:13 -0700

    Adding syncthing-relay dockerfile

---
+++ syncthing-relay/build.sh
@@ -0,0 +1,17 @@
+set -ex
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=precurse
+# image name
+IMAGE=syncthing-relay
+VERSION=`cat VERSION`
+
+if uname -m | grep armv7l; then
+  BASE_IMAGE="precurse/alpine-armhf:3.6.2"
+  ARCH=arm
+  docker build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg ARCH=${ARCH} -t $USERNAME/$IMAGE:latest .
+  docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+else
+  docker build -t $USERNAME/$IMAGE:latest .
+  docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+fi
