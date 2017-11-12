a4878b86ef4f308e1ce74aeacc53da7a369c8d95
Andrew Klaus <andrewklaus@gmail.com>
2017-11-12 12:29:10 -0700

    Removing arm build stuff and moving keys directory to entrypoint in syncthing-relay

--- syncthing-relay/build.sh
+++ syncthing-relay/build.sh
@@ -6,12 +6,5 @@ USERNAME=precurse
 IMAGE=syncthing-relay
 VERSION=`cat VERSION`
 
-if uname -m | grep armv7l; then
-  BASE_IMAGE="precurse/alpine-armhf:3.6.2"
-  ARCH=arm
-  docker build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg ARCH=${ARCH} -t $USERNAME/$IMAGE:latest .
-  docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
-else
-  docker build -t $USERNAME/$IMAGE:latest .
-  docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
-fi
+docker build -t $USERNAME/$IMAGE:latest .
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
