a9dfbd74636637badc179dac5a91a1384d868be7
Andrew Klaus <andrewklaus@gmail.com>
2017-12-07 21:27:02 -0700

    Using latest alpine version

--- syncthing/build.sh
+++ syncthing/build.sh
@@ -7,7 +7,7 @@ IMAGE=syncthing
 VERSION=`cat VERSION`
 
 if uname -m | grep armv7l; then
-  BASE_IMAGE="precurse/alpine-armhf:3.6.2"
+  BASE_IMAGE="precurse/alpine-armhf:latest"
   ARCH=arm
   docker build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg ARCH=${ARCH} -t $USERNAME/$IMAGE:latest .
   docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
--- tftpd/build.sh
+++ tftpd/build.sh
@@ -7,7 +7,7 @@ IMAGE=tftpd
 VERSION=`cat VERSION`
 
 if uname -m | grep armv7l; then
-  BASE_IMAGE="precurse/alpine-armhf:3.6.2"
+  BASE_IMAGE="precurse/alpine-armhf:latest"
   docker build --build-arg BASE_IMAGE=${BASE_IMAGE} -t $USERNAME/$IMAGE:latest .
   docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
 else
