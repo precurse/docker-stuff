b283782334a1607cb0dd555a772d86855659395b
Andrew Klaus <andrew@aklaus.ca>
2020-04-10 12:33:33 -0600

    Syncthing management listen on localhost only. Adding syncthing data to home directory

--- syncthing/update.sh
+++ syncthing/update.sh
@@ -3,5 +3,5 @@ IMAGE=syncthing
 USER=precurse
 sudo docker stop $IMAGE
 sudo docker rm $IMAGE
-sudo docker create --network=host --restart unless-stopped -p 8384:8384 -p 22000:22000 -p 21027:21027/udp -v syncthing-config:/var/syncthing -v syncthing-data:/data --name $IMAGE -it $USER/$IMAGE
+sudo docker create --network=host --restart unless-stopped -p 8384:8384 -p 22000:22000 -p 21027:21027/udp -v syncthing-config:/var/syncthing -v ${HOME}/syncthing:/data --name $IMAGE -it $USER/$IMAGE
 sudo docker start $IMAGE
