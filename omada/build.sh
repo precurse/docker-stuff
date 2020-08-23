5a6c8372ce609a9b19507e313ff62c09089e0202
Andrew Klaus <andrew@aklaus.ca>
2020-08-23 10:43:49 -0600

    Omada: Fixing Dockerfile and build.sh

--- omada/build.sh
+++ omada/build.sh
@@ -1,7 +1,7 @@
 set -ex
 # Get latest version
 URL=$(curl https://www.tp-link.com/ca/support/download/omada-software-controller/ 2>/dev/null |grep linux_x64_tar.gz | awk -F ' ' '{ print $5 }' | awk -F '"' '{ print $2 }' |head -1)
-VERSION=$(echo $URL | awk '{n=split($1,A,"/"); split(A[n],B,"_"); print B[3]}')
+VERSION=$(echo $URL | awk '{n=split($1,A,"/"); split(A[n],B,"_"); print B[4]}')
 echo $VERSION > VERSION
 
 # SET THE FOLLOWING VARIABLES
