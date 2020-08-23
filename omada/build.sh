28b0888cf4b38b5d71d66fdda356c63ca9890375
Andrew Klaus <andrew@aklaus.ca>
2020-08-23 10:12:36 -0600

    Omada: Fixing build.sh

--- omada/build.sh
+++ omada/build.sh
@@ -1,6 +1,6 @@
 set -ex
 # Get latest version
-URL=$(curl https://www.tp-link.com/us/support/download/eap245/ 2>/dev/null |grep linux_x64.tar.gz | awk -F ' ' '{ print $6 }' | awk -F '"' '{ print $2 }' |head -1)
+URL=$(curl https://www.tp-link.com/ca/support/download/omada-software-controller/ 2>/dev/null |grep linux_x64_tar.gz | awk -F ' ' '{ print $5 }' | awk -F '"' '{ print $2 }' |head -1)
 VERSION=$(echo $URL | awk '{n=split($1,A,"/"); split(A[n],B,"_"); print B[3]}')
 echo $VERSION > VERSION
 
